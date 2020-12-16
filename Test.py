7027713dbecfa77b36af5b5d1a849169bda272e9
bruhmoment <58310567+TimonBRO@users.noreply.github.com>
2020-12-16 13:04:17 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -15,5 +15,7 @@ class CalcTest(ut.TestCase):
    
         
 if __name__ == '__main__':
+    import xmlrunner
+    runner=xmlrunner.XMLTestRunner(output='test-reports')
+    ut.main(testRunner=runner)
     ut.main()
-
